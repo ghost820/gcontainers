763e4f0a567f3b83e412768d47b0afe5d3ba2fb4
Ghost <ghost@unknown.com>
2024-08-19 11:41:21 +0200

    Math

---
+++ gcontainers-test/math.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+
+#include "../gcontainers/math.h"
+
+TEST(Abs, Basic)
+{
+	ASSERT_EQ(5, Abs(5));
+	ASSERT_EQ(5, Abs(-5));
+	ASSERT_EQ(0, Abs(0));
+}
+
+TEST(Pow, Basic)
+{
+	ASSERT_EQ(1, Pow(5, 0));
+	ASSERT_EQ(5, Pow(5, 1));
+	ASSERT_EQ(25, Pow(5, 2));
+	ASSERT_EQ(125, Pow(5, 3));
+
+	ASSERT_EQ(1, Pow(-5, 0));
+	ASSERT_EQ(-5, Pow(-5, 1));
+	ASSERT_EQ(25, Pow(-5, 2));
+	ASSERT_EQ(-125, Pow(-5, 3));
+
+	ASSERT_EQ(0, Pow(0, 3));
+}
+
+TEST(GetDigitCount, Basic)
+{
+	ASSERT_EQ(1, GetDigitCount(5));
+	ASSERT_EQ(2, GetDigitCount(10));
+	ASSERT_EQ(2, GetDigitCount(99));
+	ASSERT_EQ(3, GetDigitCount(100));
+	ASSERT_EQ(3, GetDigitCount(999));
+	ASSERT_EQ(4, GetDigitCount(1000));
+	ASSERT_EQ(4, GetDigitCount(9999));
+	
+	ASSERT_EQ(1, GetDigitCount(-5));
+	ASSERT_EQ(2, GetDigitCount(-10));
+	ASSERT_EQ(2, GetDigitCount(-99));
+	ASSERT_EQ(3, GetDigitCount(-100));
+	ASSERT_EQ(3, GetDigitCount(-999));
+	ASSERT_EQ(4, GetDigitCount(-1000));
+	ASSERT_EQ(4, GetDigitCount(-9999));
+	
+	ASSERT_EQ(1, GetDigitCount(0));
+}
+
+TEST(GetDigit, Basic)
+{
+	ASSERT_EQ(5, GetDigit(12345, 0));
+	ASSERT_EQ(4, GetDigit(12345, 1));
+	ASSERT_EQ(3, GetDigit(12345, 2));
+	ASSERT_EQ(2, GetDigit(12345, 3));
+	ASSERT_EQ(1, GetDigit(12345, 4));
+	ASSERT_EQ(0, GetDigit(12345, 5));
+
+	ASSERT_EQ(5, GetDigit(-12345, 0));
+	ASSERT_EQ(4, GetDigit(-12345, 1));
+	ASSERT_EQ(3, GetDigit(-12345, 2));
+	ASSERT_EQ(2, GetDigit(-12345, 3));
+	ASSERT_EQ(1, GetDigit(-12345, 4));
+	ASSERT_EQ(0, GetDigit(-12345, 5));
+}
---
+++ gcontainers/math.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cstdint>
+
+template<typename T>
+T Abs(T v)
+{
+	return v < 0 ? -v : v;
+}
+
+template<typename T>
+T Min(T a, T b)
+{
+	return a < b ? a : b;
+}
+
+template<typename T>
+T Max(T a, T b)
+{
+	return a > b ? a : b;
+}
+
+template<typename T>
+T Pow(T base, uint64_t exp)
+{
+	if (exp == 0) {
+		return 1;
+	}
+
+	if (base == 0 || base == 1 || base == -1) {
+		return base;
+	}
+
+	T result = base;
+	for (uint64_t i = 1; i < exp; i++) {
+		result *= base;
+	}
+	return result;
+}
+
+template<typename T>
+uint64_t GetDigitCount(T num)
+{
+	if (num == 0) {
+		return 1;
+	}
+
+	uint64_t count = 0;
+	while (num != 0) {
+		num /= 10;
+		count++;
+	}
+	return count;
+}
+
+template<typename T>
+int GetDigit(T num, uint64_t n)
+{
+	return (Abs(num) / Pow(10, n)) % 10;
+}
+
+template<typename T>
+uint64_t MostDigits(T* arr, uint64_t size)
+{
+	uint64_t maxDigits = 0;
+	for (uint64_t i = 0; i < size; i++) {
+		maxDigits = Max(maxDigits, GetDigitCount(arr[i]));
+	}
+	return maxDigits;
+}
